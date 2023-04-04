8029502e234b4bcd76086a2d7ab3c65ff7f7e4ca
Pdisk <83772019+pdisk@users.noreply.github.com>
2023-04-04 10:13:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import urllib.parse
+
+from flask import Flask, Response, request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/get/<path:url>', methods=['GET'])
+def proxy(url):
+    url = urllib.parse.unquote(url)
+    print(url)
+    url = "https://api.allorigins.win/raw?url=" + url
+    response = requests.get(url, stream=True)
+    headers = {key: value for key, value in response.headers.items()}
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Content-Disposition'] = f'attachment; filename="{url.split("/")[-1]}"'
+    return Response(response.iter_content(chunk_size=1024), headers=headers,
+                    content_type=response.headers['content-type'], )
+
+if __name__ == '__main__':
+    app.run(debug=True)
